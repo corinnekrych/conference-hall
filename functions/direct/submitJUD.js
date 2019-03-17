6672ae61f976b4ce51ad1e5a2bf398439a2e07a6
Corinne Krych <corinnekrych@gmail.com>
2019-03-17 11:18:07 +0100

    test

--- functions/direct/submitJUD.js
+++ functions/direct/submitJUD.js
@@ -8,21 +8,21 @@ const { addProposal } = require('../firestore/proposal')
 // firebase functions:shell
 // and simply type submitJUD with the talk id to move.
 // firebase > submitJud()
-const submitJUD = async () => {
+const submitJUD = async (tt) => {
   // const JUDCon = 'wpYPL2EC3WzxUqY77rQZ'
   const test = 'TL1xm0zyxZG8eoXBE5mp'
   const corinne = 'ibBeWNBzL3XVc0teerodftWdYzD2'
   const talks = await getAllTalks()
   const corinneTalks = talks.filter(t => t.speakers[corinne])
-  console.log(`${JSON.stringify(corinneTalks)}`)
+  //console.log(`${corinneTalks.title}`)
   for (const talk of corinneTalks) {
     const submittedTalk = omit(talk, ['createTimestamp', 'submissions'])
     if (isEmpty(submittedTalk)) {
       console.log('Talk not found')
       return
     }
-    console.log(`Talk: ${JSON.stringify(submittedTalk)} to be submitted to Test`)
-    //await addProposal(test, submittedTalk)
+    console.log(`Talk: ${submittedTalk.title} to be submitted to Test`)
+    await addProposal(test, submittedTalk)
     console.log(`Talk: ${submittedTalk.title} submitted to Test`)
   }
 }
